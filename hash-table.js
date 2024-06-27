a5ebcff402fbe05dc59a72c6f786088906e84faf
Robert Weekes <weekes.robert@gmail.com>
2024-06-26 17:19:01 -0700

    phase 3 done

--- hash-table.js
+++ hash-table.js
@@ -44,7 +44,20 @@ class HashTable {
     }
 
     insertWithHashCollisions(key, value) {
+        const index = this.hashMod(key);  // assign index for new pair
+        console.log(index);
+        const newPair = new KeyValuePair(key, value);
+        let current = this.data[index];
 
+        // if there is already a key/value pair at that index,
+        if(current) {  // set up linked list chaining
+            console.log(current);
+            newPair.next = current;   // newPair becomes the head, bump the old "current" to the right
+            this.data[index] = newPair;   // cannot use 'current' variable on the left, must set new data point manually
+        } else {    // inserting new pair into empty bucket \/
+            this.data[index] = newPair; // assign value at new hashed index
+        }
+        this.count++;
     }
 
     insert(key, value) {
@@ -53,26 +66,37 @@ class HashTable {
 
 }
 
-// local testing
+// // local testing
+
+// // phase 1-2
+
+// hashTable = new HashTable(2);
+
+// hashTable.insertNoCollisions("key-1", "val-1");
+// hashTable.insertNoCollisions("key-2", "val-2");
+
+// console.log(hashTable);
+// console.log(hashTable.count) // (2);
+// console.log(hashTable.capacity) // (2);
+// console.log(hashTable.data.length) // (2);
+
+// const pairA = hashTable.data[0];
+// const pairB = hashTable.data[1];
+
+// console.log(pairA.key) // "key-1"
+// console.log(pairA.value) // "val-1"
+
+// console.log(pairB.key) // "key-2"
+// console.log(pairB.value) // "val-2"
+
+// // phase 3
 
 hashTable = new HashTable(2);
-hashTable.insertNoCollisions("key-1", "val-1");
-hashTable.insertNoCollisions("key-2", "val-2");
 
-console.log(hashTable);
-console.log(hashTable.count) // (2);
-console.log(hashTable.capacity) // (2);
-console.log(hashTable.data.length) // (2);
-
-const pairA = hashTable.data[0];
-const pairB = hashTable.data[1];
-
-console.log(pairA.key) // "key-1"
-console.log(pairA.value) // "val-1"
-
-console.log(pairB.key) // "key-2"
-console.log(pairB.value) // "val-2"
-
+// index: 0, 1, 0 (key3 collides with key 1, head)
+hashTable.insertWithHashCollisions("key-1", "val-1");
+hashTable.insertWithHashCollisions("key-2", "val-2");
+hashTable.insertWithHashCollisions("key-3", "val-3");
 
 
 module.exports = HashTable;
